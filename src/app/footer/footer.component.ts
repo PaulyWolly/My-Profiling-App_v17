3155e0e513f4fa1f0b51215768e8b92363506c87
Paul Welby <c205428@ardentmills.com>
2026-06-24 10:06:01 -0700

    Add footer build version stamp from latest git commit on each build.
    Shows ver M.D.YYYY.H:MM on the right side of the footer so local and Render deploys are easy to identify.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { buildVersion } from '@environments/build-version';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FooterComponent {
+  readonly buildVersion = buildVersion;
 }
